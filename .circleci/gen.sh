b4ed9df5a9925cc3c7c0466a82d778b5321f6211
Shingo Sato <shinsugar@gmail.com>
2024-05-26 22:54:59 +0900

    Update

--- .circleci/gen.sh
+++ .circleci/gen.sh
@@ -1,19 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -o nounset
 set -o errexit
 set -o pipefail
 
-readonly DIARY_REPO_NAME=diary
-readonly WORKSPACE=$DIARY_REPO_NAME
-readonly TARGET_DIR=$(date +%Y/%m)
-readonly FILE_PATH=${TARGET_DIR}/$(date +%d).md
+DIARY_REPO_NAME=diary
+WORKSPACE=$DIARY_REPO_NAME
+TARGET_DIR=$(date +%Y/%m)
+FILE_PATH=${TARGET_DIR}/$(date +%d).md
 
-[ -d "$WORKSPACE" ] || git clone --depth 1 git@github.com:sugarshin/"$DIARY_REPO_NAME".git "$WORKSPACE"
-cd "$WORKSPACE"
+[ -d "${WORKSPACE}" ] || git clone --depth 1 "git@github.com:sugarshin/${DIARY_REPO_NAME}".git "${WORKSPACE}"
+cd "${WORKSPACE}"
 git pull --depth=1 || true
-mkdir -p "$TARGET_DIR"
-echo -e "# $(date '+%A %B %d %Y')\n" > "$FILE_PATH"
-git add "$FILE_PATH"
+if [[ -f "${FILE_PATH}" ]]; then
+  echo Already exists.
+  exit 0
+fi
+mkdir -p "${TARGET_DIR}"
+echo -e "# $(date '+%A %B %d %Y')\n" > "${FILE_PATH}"
+git add "${FILE_PATH}"
 git commit -m "Add ${FILE_PATH}"
 git push origin
